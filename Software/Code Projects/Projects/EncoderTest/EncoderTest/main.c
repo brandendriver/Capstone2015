36d008b138cccacdee460f94f394ada624a079b0
andrewbrain <andrews5@pdx.edu>
2015-04-17 15:48:17 -0700

    Updating encoder code to V2

--- Software/Code Projects/Projects/EncoderTest/EncoderTest/main.c
+++ Software/Code Projects/Projects/EncoderTest/EncoderTest/main.c
@@ -1,31 +1,33 @@
+#ifndef _LINUX
+#include <windows.h>
+#include <conio.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+#ifndef _LINUX
 #include "826api.h"
+#else
+#include "826api.h"
+#endif
 
 static int LinearVelocity(uint brd, uint chan){
-	uint rad = 1;															//radius of wheel attached to the encoder in inches
+	float rad = .975;															//radius of wheel attached to the encoder in inches
+	int tmax = 1000000;											// max interval time in microseconds, this will wait until event edge event is detected
 	uint val = 0;
-	uint *value;															//create pointer to direct counts buffer value to val
-	value = &val;
+	//uint *value;
+//	value = &val;
+	uint timestamp, reason;
 
-	uint tstamp = NULL;														//set null to ignore the timestamp	
-	uint *timestamp;
-	timestamp = &tstamp;
-
-	uint rsn = NULL;														//set null to ignore the reason
-	uint *reason;
-	reason = &rsn;
-
-	int tmax = S826_WAIT_INFINITE;											// max interval time in microseconds, this will wait until event edge event is detected
-	S826_CounterSnapshotRead(brd, chan, &value, &timestamp, &reason, tmax); //Obtain a snapshot of what the counts buffer has after tmax time
-
-	val = *value;															//begin calculating Linear Velocity	
+	S826_CounterSnapshotRead(brd, chan, &val, &timestamp, &reason, tmax); //Obtain a snapshot of what the counts buffer has after tmax time
+										
 	double pi = 3.1415;
 	float numerator = val * 2 * pi * rad;									// calculate the numerator value for linear velocity
 	int fullrotation = 5000;												// nuber of counts in a full rotation of the encoder
 	float lin = numerator / fullrotation;									// calculates the linear velocity of wirefeed in inches/second
-	pintf("linear velocity = %.2f\n", lin);									// display the current linear velocity, should update every second.
+	printf("linear velocity = %.2f\n", lin);									// display the current linear velocity, should update every second.
 }
 
 int main(void){
@@ -48,9 +50,11 @@ int main(void){
 		printf("TARGET BOARD NOT FOUND\n");								   // driver didn't find board you want to use
 	else
 	{
+		S826_CounterStateWrite(brd, chan, 0);
 		S826_CounterModeWrite(brd, chan, ctrmode);						   // use the CounterModeWrite function to set the mode of the counter (in this case the mode is set to frequency measurement)
 		S826_CounterSnapshotConfigWrite(brd, chan, 0x10, 0);			   // use the CounterSnapshotConfigureWrite to configure the snapshot collection on our counter
 		S826_CounterPreloadWrite(brd, chan, preload0reg, PLcount);		   // use the CounterPreloadWrite function to pre-load 0 into the counts buffer.
+		S826_CounterStateWrite(brd, chan, 1);
 		while (k == 1){													   // begin infinite loop
 			LinearVelocity(brd, chan);									   // call the linear velocity function
 		}
