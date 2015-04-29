fbeb3b85023648e89b2e9a8240db806d9ab9cdda
ctribe <tribe.cameron@gmail.com>
2015-04-28 20:17:55 -0700

    added code for droplet spacing test

--- Software/Code Projects/Projects/AnalogTest/AnalogTest/analogTest.c
+++ Software/Code Projects/Projects/AnalogTest/AnalogTest/analogTest.c
@@ -14,91 +14,14 @@
 #include <math.h>
 
 #ifndef _LINUX
-#include "..\826api.h"
+#include "826api.h"
 #else
 #include "826api.h"
 #endif
 
 
-// Helpful macros for DIOs
-#define DIO(C)                  ((uint64)1 << (C))                          // convert dio channel number to uint64 bit mask
-#define DIOMASK(N)              {(uint)(N) & 0xFFFFFF, (uint)((N) >> 24)}   // convert uint64 bit mask to uint[2] array
-#define DIOSTATE(STATES,CHAN)   ((STATES[CHAN / 24] >> (CHAN % 24)) & 1)    // extract dio channel's boolean state from uint[2] array
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// ERROR HANDLING
-// These examples employ very simple error handling: if an error is detected, the example functions will immediately return an error code.
-// This behavior may not be suitable for some real-world applications but it makes the code easier to read and understand. In a real
-// application, it's likely that additional actions would need to be performed. The examples use the following X826 macro to handle API
-// function errors; it calls an API function and stores the returned value in errcode, then returns immediately if an error was detected.
 
 #define X826(FUNC)   if ((errcode = FUNC) != S826_ERR_OK) { printf("\nERROR: %d\n", errcode); return errcode;}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Counter utility functions used by the demos.
-
-// PERIODIC TIMER --------------------
-
-// Counter mode: count down at 1MHz, preload upon start or when 0 reached, assert ExtOut when not 0
-#define TMR_MODE  (S826_CM_K_1MHZ | S826_CM_UD_REVERSE | S826_CM_PX_ZERO | S826_CM_PX_START | S826_CM_OM_NOTZERO)
-
-// Configure a counter channel to operate as a periodic timer and start it running.
-static int PeriodicTimerStart(uint board, uint counter, uint period)
-{
-    int errcode;
-    X826( S826_CounterStateWrite(board, counter, 0)                         );     // halt channel if it's running
-    X826( S826_CounterModeWrite(board, counter, TMR_MODE)                   );     // configure counter as periodic timer
-    X826( S826_CounterSnapshotConfigWrite(board, counter, 4, S826_BITWRITE) );     // capture snapshots at zero counts
-    X826( S826_CounterPreloadWrite(board, counter, 0, period)               );     // timer period in microseconds
-    X826( S826_CounterStateWrite(board, counter, 1)                         );     // start timer
-    return errcode;
-}
-
-// Halt channel operating as periodic timer.
-static int PeriodicTimerStop(uint board, uint counter)
-{
-    return S826_CounterStateWrite(board, counter, 0);
-}
-
-// Wait for periodic timer event.
-static int PeriodicTimerWait(uint board, uint counter, uint *timestamp) 
-{
-    uint counts, tstamp, reason;    // counter snapshot
-    int errcode = S826_CounterSnapshotRead(board, counter, &counts, &tstamp, &reason, S826_WAIT_INFINITE);    // wait for timer snapshot
-    if (timestamp != NULL)
-        *timestamp = tstamp;
-    return errcode;
-}
-
-// PWM GENERATOR -----------------------
-
-// Counter mode: count down at 1MHz, preload when 0 reached, use both preload registers, assert ExtOut when Preload1 is active
-#define PWM_MODE  (S826_CM_K_1MHZ | S826_CM_UD_REVERSE | S826_CM_PX_ZERO | S826_CM_PX_START | S826_CM_BP_BOTH | S826_CM_OM_PRELOAD)
-
-//#define PWM_MODE 0x01682020
-
-// Configure a counter channel to operate as a pwm generator and start it running.
-static int PwmGeneratorStart(uint board, uint counter, uint ontime, uint offtime)
-{
-    int errcode;
-    X826( S826_CounterStateWrite(board, counter, 0)                         );  // halt counter channel if it's running
-    X826( S826_CounterModeWrite(board, counter, PWM_MODE)                   );  // configure counter as pwm generator
-    X826( S826_CounterSnapshotConfigWrite(board, counter, 0, S826_BITWRITE) );  // don't need counter snapshots -- we're just outputting pwm signal
-    X826( S826_CounterPreloadWrite(board, counter, 0, offtime)              );  // program pwm on-time in microseconds
-    X826( S826_CounterPreloadWrite(board, counter, 1, ontime)               );  // program pwm off-time in microseconds
-    X826( S826_CounterStateWrite(board, counter, 1)                         );  // start pwm generator
-    return errcode;
-}
-
-// Halt channel operating as pwm generator.
-static int PwmGeneratorStop(uint board, uint counter)
-{
-    return S826_CounterStateWrite(board, counter, 0);
-}
-
-
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -115,8 +38,8 @@ static int DemoAnalogOversample(uint board)
         uint tsettle;   // settling time in microseconds
     } attr[16] = {                                                          // during each adc burst:
         {0, 50}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0}, {0, 0},    //   switch to channel 0, wait 50us, digitize eight times
-        {1, 50}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}};   //   switch to channel 1, wait 50us, digitize eight times
-
+        {1, 50}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0}};   //  
+	x
     int i;
     int slot;
     uint slotlist;
@@ -150,69 +73,6 @@ static int DemoAnalogOversample(uint board)
     X826( S826_AdcEnableWrite(board, 0)                         );  // halt adc conversions
     return errcode;
 }
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// DEMO: DUPLICATE ANALOG INPUT WAVEFORM ON ANALOG OUTPUT
-// Resources: Analog input channel, analog output channel, counter channel.
-// This demo periodically samples an analog input (via counter-driven hardware trigger) and reproduces it on an analog output.
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Helper: Read data from one slot
-static int AdcReadSlot(uint board, uint slot, int *slotdata)
-{
-    int adcbuf[16];
-    uint slotlist = 1 << slot;                                                      // set up the adc slot list; we are only interested in one slot
-    int errcode = S826_AdcRead(board, adcbuf, NULL, &slotlist, 1000);                     // wait for data to arrive on the slot of interest (in response to adc hardware trigger)
-    if (errcode == S826_ERR_MISSEDTRIG)      // this application doesn't care about adc missed triggers
-        errcode = S826_ERR_OK;
-    *slotdata = adcbuf[slot] & 0xFFFF;                                                       // copy adc data to slotdata buffer
-
-    return errcode;
-}
-
-// The demo
-static int DemoAnalogTrack(uint board)
-{
-    // Configuration constants.
-    uint ain        = 0;        // analog input channel to track
-    uint aout       = 0;        // output duplicate waveform on this dac channel
-    uint slot       = 0;        // use this adc timeslot (the "slot of interest") to sample the analog input
-    uint tsample    = 100;      // sampling interval in microseconds
-    uint duration   = 10;       // track for this many seconds
-    uint counter    = 0;        // use this counter channel to periodically trigger adc conversions
-
-    int slotdata;               // adc data from the slot of interest
-    int errcode;
-    uint i;
-
-    printf("\nDemoAnalogTrack\n");
-
-    duration *= (1000000 / tsample);            // convert tracking time units from seconds to equivalent sampling ticks
-    
-    // Configure interfaces and start them running.
-    X826( S826_DacRangeWrite(board, aout, S826_DAC_SPAN_10_10, 0)       );  // program dac output range: -10V to +10V
-    X826( S826_AdcSlotConfigWrite(board, slot, ain, 0, S826_ADC_GAIN_1) );  // program adc timeslot attributes: slot, chan, 0us settling time
-    X826( S826_AdcSlotlistWrite(board, 1 << slot, S826_BITWRITE)        );  // enable adc timeslot; disable all other slots
-    X826( S826_AdcTrigModeWrite(board, 0x80 | (48 + counter))           );  // enable adc hardware triggering, use counter channel's ExtOut as trigger
-    X826( S826_AdcEnableWrite(board, 1)                                 );  // enable adc conversions
-    X826( PeriodicTimerStart(board, counter, tsample)                   );  // start periodic sample timer
-    X826( AdcReadSlot(board, slot, &slotdata)                           );  // discard first adc data sample (because it might need some settling time)
-		
-    for (i = 0; i < duration; i++)                                          // repeat until tracking time elapses:
-    {	
-        X826( AdcReadSlot(board, slot, &slotdata)                   );          // wait for adc data sample
-        X826( S826_DacDataWrite(board, aout, (short)slotdata + 0x8000, 0)  );   // output dac sample - cast to short to get sign
-    }
-
-    X826( S826_AdcEnableWrite(board, 0)                                 );  // halt adc conversions
-    X826( PeriodicTimerStop(board, counter)                             );  // halt periodic timer
-    return errcode;
-}
-
-
-
- 
-
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ Software/Code Projects/Projects/Wire Speed Droplet Spacing Test/Wire Speed Droplet Spacing Test/WireFeedDropletSpacingTest.c
@@ -0,0 +1,217 @@
+#ifndef _LINUX
+#include <windows.h>
+#include <conio.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stdint.h>
+
+#ifndef _LINUX
+#include "826api.h"
+#else
+#include "826api.h"
+#endif
+
+
+
+#define X826(FUNC)   if ((errcode = FUNC) != S826_ERR_OK) { printf("\nERROR: %d\n", errcode); return errcode;}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Functions
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+static int AnalogVal_Chan0(uint board, double *AdcVal)
+{
+	// Adc timeslot attributes.
+	// In each adc burst, convert analog channel 0 eight times then channel 1 eight times. Allow each channel to settle for 50 microseconds
+	// after switching (before first conversion); subsequent conversions on the same channel don't need additional settling time.
+	struct SLOTATTR {
+		uint chan;      // analog input channel
+		uint tsettle;   // settling time in microseconds
+	} 
+	attr[8] = {                                                          // during each adc burst:
+		{ 0, 50 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 } };    //   switch to channel 0, wait 50us, digitize eight times   //  
+
+	int i;
+	int slot;
+	uint slotlist;
+	int adcbuf[16];
+	int errcode;
+
+
+	// Program attributes for all timeslots.
+	for (slot = 0; slot < 8; slot++)
+		X826(S826_AdcSlotConfigWrite(board, slot, attr[slot].chan, attr[slot].tsettle, S826_ADC_GAIN_1));
+
+	// Configure adc system and start it running.
+	X826(S826_AdcSlotlistWrite(board, 0xFFFF, S826_BITWRITE));  // enable all timeslots
+	X826(S826_AdcTrigModeWrite(board, 0));  // select continuous (untriggered) mode
+	X826(S826_AdcEnableWrite(board, 1));  // enable conversions
+
+	// Fetch and display oversampled data.
+
+	//	for (i = 0; i < 12; i++)                                        // repeat a few times:
+		//{
+			int sum[] = { 0, 0 };                                         // zero the adc sample accumulators, one per analog input channel
+			slotlist = 0xFFFF;
+			X826(S826_AdcRead(board, adcbuf, NULL, &slotlist, 1000)); // read adc data from 16 slots
+
+			for (slot = 0; slot < 16; slot++)                           // sum the 8 samples from each analog input 
+				sum[slot >> 3] += (short)(adcbuf[slot] & 0xFFFF);		// masking off sample meta-data
+
+			*AdcVal = ((double)(sum[0] * 10) / (8 * 32768));
+			//printf("CH0=%3.3fV\n", (double)(sum[0] * 10) / (8 * 32768));     // compute average values, report measured voltages
+//		}
+	
+
+	X826(S826_AdcEnableWrite(board, 0));  // halt adc conversions
+	return errcode;
+}
+
+static int ElapsedTime(uint board, uint TimeStampOld, uint *time){
+	uint64_t Tmax = 4294967296;
+	uint TimeStampNew;
+
+	S826_TimestampRead(board, &TimeStampNew);
+	if (TimeStampOld > TimeStampNew){
+		*time = Tmax - TimeStampOld + TimeStampNew;
+	}
+	else{
+		*time = (TimeStampNew - TimeStampOld); // / 1000;
+		//printf("time is: %u", time);
+	}
+
+}
+
+static int WireSpeed(uint brd){
+	
+	float rad = .975;															//radius of wheel attached to the encoder in inches
+	int tmax = 1000000;											// max interval time in microseconds, this will wait until event edge event is detected
+	uint val = 0;
+	uint timestamp, reason;
+	uint chan = 0;
+
+	S826_CounterSnapshotRead(brd, chan, &val, &timestamp, &reason, tmax); //Obtain a snapshot of what the counts buffer has after tmax time
+
+	double pi = 3.1415;
+	float numerator = val * 2 * pi * rad;									// calculate the numerator value for linear velocity
+	int fullrotation = 5000;												// nuber of counts in a full rotation of the encoder
+	float lin = numerator / fullrotation;									// calculates the linear velocity of wirefeed in inches/second
+	printf("Wire Feed = %.2f in/sec\t" , lin);									// display the current linear velocity, should update every second.
+}
+
+static int AveragePeakTime(uint Peaks, uint SampleTime) {
+	float PeakAvg;
+
+	PeakAvg = SampleTime / Peaks;
+	printf("Average Peak Time %u\n", PeakAvg);
+
+
+}
+
+static int CounterIni(uint brd){											//Function for initializing counter in frequency measurement mode
+	uint chan = 0;															// counter channel number
+	uint ctrmode = 0x00008009;												// counter mode
+	uint PLcount = 0;														// Pre-Load count for resetting the counter after an event
+	uint preload0reg = 0;
+
+	S826_CounterStateWrite(brd, chan, 0);								//Deactive counter
+	S826_CounterModeWrite(brd, chan, ctrmode);						   // use the CounterModeWrite function to set the mode of the counter (in this case the mode is set to frequency measurement)
+	S826_CounterSnapshotConfigWrite(brd, chan, 0x10, 0);			   // use the CounterSnapshotConfigureWrite to configure the snapshot collection on our counter
+	S826_CounterPreloadWrite(brd, chan, preload0reg, PLcount);		   // use the CounterPreloadWrite function to pre-load 0 into the counts buffer.
+	S826_CounterStateWrite(brd, chan, 1);								//Activate counter
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Main function.
+
+int main(void)
+{
+	uint brd = 0;                        // change this if you want to use other than board number 0
+	uint TimeStampOld;
+	uint time = 0;
+	uint RunTime = 0;
+	uint Peaks = 0;
+	uint PeakAvg;
+	double AdcValOld = 0;
+	double AdcValNew = 0;
+
+
+	
+	int errcode = S826_ERR_OK;
+	int boardflags = S826_SystemOpen();        // open 826 driver and find all 826 boards
+
+	if (boardflags < 0)
+		errcode = boardflags;                       // problem during open
+	else if ((boardflags & (1 << brd)) == 0)
+		printf("TARGET BOARD NOT FOUND\n");         // driver didn't find board you want to use
+	//////////////////////////////////////////////////////////////////////////////////
+	//////////Main Loop Starts Here///////////////////////////////////////////////////
+	//////////////////////////////////////////////////////////////////////////////////
+	else
+	{
+		
+
+		AnalogVal_Chan0(brd, &AdcValOld);
+		while(RunTime < 40000000){
+			//WireSpeed(brd);									//measure and display wirespeed
+			uint TimeStampNew = 0;
+			S826_TimestampRead(brd, &TimeStampOld);				//gets time stamp at beginning of program
+
+			while(time <= 8000000){
+				AnalogVal_Chan0(brd, &AdcValNew);
+				float Threshold = AdcValOld - .6;
+				if (AdcValNew < Threshold){
+					Peaks++;
+				}
+
+				AdcValOld = AdcValNew;
+				ElapsedTime(brd, TimeStampOld, &time);
+			}
+			//AveragePeakTime(Peaks, &PeakAvg, T1);
+			if (Peaks != 0){
+				printf("Average Peak Time %u\t", time / Peaks);
+				printf("Number of Peaks %u\n", Peaks);
+			}
+			else{
+				printf("Error, no peaks detected\n");
+			}
+			RunTime = RunTime + time;
+			time = 0;
+			Peaks = 0;
+		}
+		
+	
+
+	}
+
+	switch (errcode)
+	{
+	case S826_ERR_OK:           break;
+	case S826_ERR_BOARD:        printf("Illegal board number"); break;
+	case S826_ERR_VALUE:        printf("Illegal argument"); break;
+	case S826_ERR_NOTREADY:     printf("Device not ready or timeout"); break;
+	case S826_ERR_CANCELLED:    printf("Wait cancelled"); break;
+	case S826_ERR_DRIVER:       printf("Driver call failed"); break;
+	case S826_ERR_MISSEDTRIG:   printf("Missed adc trigger"); break;
+	case S826_ERR_DUPADDR:      printf("Two boards have same number"); break; S826_SafeWrenWrite(brd, 0x02);
+	case S826_ERR_BOARDCLOSED:  printf("Board not open"); break;
+	case S826_ERR_CREATEMUTEX:  printf("Can't create mutex"); break;
+	case S826_ERR_MEMORYMAP:    printf("Can't map board"); break;
+	default:                    printf("Unknown error"); break;
+	}
+
+
+#ifndef _LINUX	
+	printf("\nKeypress to exit ...\n\n");
+	while (!_kbhit());
+	_getch();
+#endif
+
+	S826_SystemClose();
+	return 0;
+}
+
